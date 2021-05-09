090499075fbaa128ffa4ef05bf79d7e9760065ef
biruk <birukmolla30@gmail.com>
2021-05-08 16:17:56 -1200

    first commit

---
+++ lib/HotelPage.dart
@@ -0,0 +1,265 @@
+import 'package:flutter/material.dart';
+import 'package:food_ordering_app/style.dart';
+
+class HotelPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "mont"),
+      home: hotelpage(),
+    );
+  }
+}
+
+class hotelpage extends StatefulWidget {
+  @override
+  _hotelpageState createState() => _hotelpageState();
+}
+
+class _hotelpageState extends State<hotelpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: blue,
+                      image: DecorationImage(
+                          image: AssetImage("asset/images/hotelBig.png"),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                              ),
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                Icons.search,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 200,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Yoshinasa Sushi",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 20),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Icon(Icons.star, color: Colors.white),
+                                    Icon(Icons.star, color: Colors.white),
+                                    Icon(Icons.star, color: Colors.white),
+                                    Icon(Icons.star, color: Colors.white),
+                                    Icon(Icons.star, color: Colors.white),
+                                    Text(
+                                      "250 Reviews",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 13,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                            Container(
+                              height: 70,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.white),
+                              child: Center(
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: Colors.redAccent,
+                                  size: 30,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "lorem ipsum this is one of the most testy food",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text("Today's Special", style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                            Expanded(
+                              child: Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 20),
+                                  height: 0.5,
+                                  color: Colors.grey,
+                                )
+                              ),
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        placesWidget("food1", "sandwich"),
+                        SizedBox(height: 20,),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text("Dishes", style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                            Expanded(
+                              child: Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 20),
+                                  height: 0.5,
+                                  color: Colors.grey,
+                                )
+                              ),
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+   Row placesWidget(String img, String name) {
+    return Row(
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("asset/images/$img.png"),
+          )),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "$name",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+              Text(
+                "Lorem ipsum sits amet is for publishing",
+                style: TextStyle(fontSize: 12),
+              ),
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: (){},
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(40)),
+                color: greenBtn),
+            child: Text(
+              "order Now",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
